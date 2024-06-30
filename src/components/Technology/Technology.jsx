0c6e61dcd5855c0610a2c819f3a8b2ee651373fe
GarvGopalia <92243018+GarvGopalia@users.noreply.github.com>
2024-06-30 16:40:28 +0530

    complete technology module

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useState}from 'react';
 import "./Technology.css";
+import techData from './TechnologyData';
 
 const Technology = () => {
+    const [tech, setTech] = useState("Vehicle")
     return (
         <div className='body-tech'>
-            <h1></h1>
+            <p className='tech-head'>
+                <span className='tech-head-span'>03</span> &nbsp; SPACE LAUNCH 101
+            </p>
+            <div className="tech-content-container">
+                <div className="tech-content">
+                    <div className="tech-button">
+                        <button className='tech-btn'
+                            id='Vehicle' onClick={() => {
+                                setTech("Vehicle")
+                            }}>
+                                1
+                        </button>
+                        <button className='tech-btn'
+                            id='Capsule' onClick={() => {
+                                setTech("Capsule")
+                            }}>
+                                2
+                        </button>
+                        <button className='tech-btn'
+                            id='Port' onClick={() => {
+                                setTech("Port")
+                            }}>
+                                3
+                        </button>
+                    </div>
+                    <div className="tech-details">
+                        <h3 className='tech-name'>{techData[tech].name}</h3>
+                        <h1 className='tech-title'>{techData[tech].title}</h1>
+                        <p className='tech-description'>{techData[tech].description}</p>
+                    </div>
+                </div>
+                <div className="techimage">
+                    <img className='image' src={techData[tech].image} alt={"Image of" + tech} />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/Technology/TechnologyData.js
@@ -0,0 +1,22 @@
+const techData = {
+    "Vehicle": {
+        "title": "Launch vehicle",
+        "name": "the terminology...",
+        "description": "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall,it's quite an awe-inspiring sight on the launch pad!",
+        "image" : "https://raw.githubusercontent.com/GarvGopalia/Space/main/src/assets/technology/image-launch-vehicle-portrait.jpg"
+    },
+    "Capsule": {
+        "title": "Space capsule",
+        "name": "the terminology...",
+        "description": "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+        "image": "https://raw.githubusercontent.com/GarvGopalia/Space/main/src/assets/technology/image-space-capsule-portrait.jpg"
+    },
+    "Port": {
+        "title": "Spaceport",
+        "name": "the terminology...",
+        "description": "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+        "image": "https://raw.githubusercontent.com/GarvGopalia/Space/main/src/assets/technology/image-spaceport-portrait.jpg"
+    },
+}
+
+export default techData;
